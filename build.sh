75b3a884a4059145f02d353af81d3846decbc109
Felipe Cabrera <fecabrera@protonmail.com>
2026-07-22 13:28:38 -0400

    Add support for building example files in the build script and update .gitignore to include example directories while preserving .sie files.

--- build.sh
+++ build.sh
@@ -30,4 +30,22 @@ run_echo $SIEC \
     ${SIE_LIB_OBJS[@]}\
     -o dist/bin/sie
 
+# Build examples
+for pkg in $(find packages -type d -mindepth 1 -maxdepth 1); do
+    mkdir -p dist/$(basename $pkg)/examples/
+    for dir in $(find $pkg/examples -type d -mindepth 1 -maxdepth 1); do
+        mkdir -p dist/$(basename $pkg)/examples/$(basename $dir)/
+        for f in $(find $dir -mindepth 1 -maxdepth 1 -name "*.sie"); do
+            run_echo $SIEC \
+                "${SIE_INCLUDES[@]/#/-I }"\
+                "${SIE_LIB_DIRS[@]/#/-L }"\
+                "${SIE_LINK_LIBS[@]/#/-l }"\
+                ${SIE_FLAGS}\
+                $f \
+                ${SIE_LIB_OBJS[@]}\
+                -o dist/$(basename -- $pkg)/examples/$(basename -- $dir)/$(basename -- ${f%.sie})
+        done
+    done
+done
+
 run_echo pip wheel . --no-deps -w dist
