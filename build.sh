d05e1e8ca2fda763c1e0a8f98f733993bbadd2ac
Felipe Cabrera <fecabrera@protonmail.com>
2026-07-22 15:52:01 -0400

    Refactor build script to ensure 'dist/bin/' directory is created before building examples, and update example files to include 'mpd_context' import and adjust function signatures for consistency with stdio types.

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ run_echo() {
 }
 
 mkdir -p dist/
-mkdir -p dist/bin/
 
 # Build tomlc17
 run_echo cd tomlc17
@@ -21,6 +20,7 @@ run_echo cp src/libtomlc17.a ../dist/
 run_echo cd ..
 
 # Build sie
+mkdir -p dist/bin/
 run_echo $SIEC \
     "${SIE_INCLUDES[@]/#/-I }"\
     "${SIE_LIB_DIRS[@]/#/-L }"\
@@ -33,6 +33,17 @@ run_echo $SIEC \
 # Build examples
 for pkg in $(find packages -type d -mindepth 1 -maxdepth 1); do
     mkdir -p dist/$(basename $pkg)/examples/
+    for f in $(find $pkg/examples -mindepth 1 -maxdepth 1 -name "*.sie"); do
+        run_echo $SIEC \
+            "${SIE_INCLUDES[@]/#/-I }"\
+            "${SIE_LIB_DIRS[@]/#/-L }"\
+            "${SIE_LINK_LIBS[@]/#/-l }"\
+            ${SIE_FLAGS}\
+            $f \
+            ${SIE_LIB_OBJS[@]}\
+            -o dist/$(basename -- $pkg)/examples/$(basename -- ${f%.sie}) 
+    done
+    
     for dir in $(find $pkg/examples -type d -mindepth 1 -maxdepth 1); do
         mkdir -p dist/$(basename $pkg)/examples/$(basename $dir)/
         for f in $(find $dir -mindepth 1 -maxdepth 1 -name "*.sie"); do
